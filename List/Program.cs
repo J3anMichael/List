b6092cb7ea8ada540417cbb757fbb63ab660918c
Jean Michael <jeanmmclaudino@gmail.com>
2022-03-18 10:53:45 -0300

    Add project files.

---
+++ List/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> values = new List<int>();
+
+            values.Add(14);
+            values.Add(4);
+            values.Add(78);
+            values.Add(90);
+
+            int[] numbers = new int[] { 12, 45, 89 };
+            values.AddRange(numbers);
+
+            Console.WriteLine("A lista possui: " + values.Count + " elementos");
+            Console.ReadLine();
+        }
+    }
+}
